a0d6a714132fcc6cb9821892657e5de025608b35
e33io <info@e33.io>
2025-12-13 19:26:33 -0600

    Update commands and comments

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# clone scripts repo
 git clone https://github.com/e33io/scripts $HOME/scripts
 
+# run distro specific i3 install script
 if [ -f "/etc/pacman.conf" ]; then
     sh $HOME/scripts/arch-install-i3.sh
 elif [ -f "/etc/debian_version" ]; then
     sh $HOME/scripts/debian-install-i3.sh
+fi
+
+# clean up user directory
+if [ -f "$HOME/.install-info" ]; then
+    rm -rf $HOME/i3-install
 fi
